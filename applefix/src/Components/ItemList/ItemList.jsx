beecf1e6cdc1d65099452730ecb33bc30d2f3e2a
Gastón A. Delucchi <gaston@mbp-de-gaston.lan>
2022-12-08 20:48:53 -0300

    corrigiendo Pre-entrega

--- applefix/src/Components/ItemList/ItemList.jsx
+++ applefix/src/Components/ItemList/ItemList.jsx
@@ -1,9 +1,36 @@
 // Imports
+import Item from "../Item"
+import '../Item/Item.css'
+import { productos, categorias } from "../../mock"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 
 // Code
 const ItemList = () => {
+    const [item, setItem] = useState(productos)
+    const { id } = useParams()
+
+    const filterCategory = new Promise((resolve) => {
+        setTimeout(() => {
+            let newProductos = productos.filter((p) => p.category === id)
+            resolve(newProductos)
+        }, 200);
+    })
+
+    useEffect(() => {
+        filterCategory.then((response) => {
+            setItem(response)
+        })
+    }, [id])
+
     return (
-        <h1>Soy el ItemList</h1>
+        <div className="ItemList">
+            {
+                item.map((producto) => {
+                    return <Item producto={producto} key={producto.id} />
+                })
+            }
+        </div>
     )
 }
 
--- applefix/src/Components/ItemListContainer/ItemListContainer.jsx
+++ applefix/src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,35 +1,11 @@
 // Imports
-import Item from "../Item/Item"
-import '../Item/Item.css'
-import { productos } from "../../mock"
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import ItemList from '../ItemList'
 
 // Code
 const ItemListContainer = () => {
-    const [item, setItem] = useState(productos)
-    const { id } = useParams()
-
-    const filterCategory = new Promise((resolve) => {
-        setTimeout(() => {
-            let newProductos = productos.filter((p) => p.category === id)
-            resolve(newProductos)
-        }, 200);
-    })
-
-    useEffect(() => {
-        filterCategory.then((response) => {
-            setItem(response)
-        })
-    }, [id])
-
     return (
         <div className='ItemListContainer'>
-            {
-                item.map((producto) => {
-                    return <Item producto={producto} key={producto.id} />
-                })
-            }
+            <ItemList />
         </div>
     )
 }
